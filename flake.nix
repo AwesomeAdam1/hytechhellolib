b6df643087a94f9c63e22c2adcec3908574d3786
AwesomeAdam1 <adamwang.aw.2006@gmail.com>
2025-10-30 17:10:19 -0400

    fixed flake overlay

--- flake.nix
+++ flake.nix
@@ -7,19 +7,21 @@
   };
 
   outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
+    let
+      overlay = final: prev: {
+        hellolib = prev.callPackage ./default.nix { };
+      };
+    in
+    (utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
         libPkg = pkgs.callPackage ./default.nix { };
       in {
         packages.default = libPkg;
-
-        overlays.default = final: prev: {
-          hellolib = prev.callPackage ./default.nix { };
-        };
-
         devShells.default = pkgs.mkShell {
           packages = [ pkgs.cmake ];
         };
-      });
+      })) // {
+        overlays = { default = overlay; };
+      };
 }
